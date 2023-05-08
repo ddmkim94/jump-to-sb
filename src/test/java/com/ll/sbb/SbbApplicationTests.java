6b6078d1702951f98e90af7b61b0ff441e5e80d1
ddmkim94 <ddmkim94@gmail.com>
2023-05-08 19:08:55 +0900

    findAll() 메서드 테스트
    Spring Data JPA는 JPA를 좀 더 쉽게 사용할 수 있다.
    CRUD 관련 메서드들이 미리 만들어져있음.

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -1,10 +1,14 @@
 package com.ll.sbb;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -25,6 +29,14 @@ class SbbApplicationTests {
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         questionRepository.save(q2);  // 두번째 질문 저장
+    }
+
+    @Test
+    void testJpa2() {
+        List<Question> all = questionRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        assertThat(all.get(0).getSubject()).isEqualTo("sbb가 무엇인가요?");
     }
 
 }
