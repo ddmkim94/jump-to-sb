b65a077204c41a18816971b23e202827c3deb597
ddmkim94 <ddmkim94@gmail.com>
2023-05-08 14:48:56 +0900

    스트림으로 구구단 구현

--- src/main/java/com/ll/sbb/MainController.java
+++ src/main/java/com/ll/sbb/MainController.java
@@ -2,7 +2,11 @@ package com.ll.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
@@ -11,5 +15,15 @@ public class MainController {
     @ResponseBody
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String multiply(@RequestParam(defaultValue = "2") int dan, @RequestParam(defaultValue = "9") int limit) {
+        System.out.println("dan = " + dan);
+        System.out.println("limit = " + limit);
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(dan, i, dan * i))
+                .collect(Collectors.joining("<br/>"));
     }
 }
