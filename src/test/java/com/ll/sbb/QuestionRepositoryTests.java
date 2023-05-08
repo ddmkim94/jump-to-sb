c5fb7cfee8988d0b37d8dea23c0e8d6a72dbfd22
ddmkim94 <ddmkim94@gmail.com>
2023-05-08 23:33:01 +0900

    도메인 별로 파일 분류

--- src/test/java/com/ll/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/sbb/QuestionRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.ll.sbb;
 
+import com.ll.sbb.question.Question;
+import com.ll.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
