5e03eedc324711c04bda19384699e3e8b9d39764
ddmkim94 <ddmkim94@gmail.com>
2023-05-09 17:22:54 +0900

    답변 리스트 구현

---
+++ src/main/java/com/ll/sbb/answer/AnswerController.java
@@ -0,0 +1,29 @@
+package com.ll.sbb.answer;
+
+import com.ll.sbb.question.Question;
+import com.ll.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(@PathVariable int id ,String content) {
+        Question question = questionService.getQuestion(id);
+
+        answerService.createAnswer(question, content);
+
+        return "redirect:/question/detail/%s".formatted(id);
+    }
+}
--- src/main/java/com/ll/sbb/question/Question.java
+++ src/main/java/com/ll/sbb/question/Question.java
@@ -26,8 +26,13 @@ public class Question {
     // mappedBy 에는 해당 필드와 매핑되는 필드의 이름을 작성 @ManyToOne 필드
     // CascadeType.REMOVE 옵션은 부모 객체(Question)가 삭제될 때 자식 객체(Answer)도 함께 삭제하는 옵션이다.
     // 질문이 없으면 답변도 없기 때문에 질문 테이블이 부모, 답변 테이블이 자식 테이블이 된다.
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answerList = new ArrayList<>();
 
     private LocalDateTime createDate;
+
+    public void addAnswer(Answer answer) {
+        this.answerList.add(answer);
+        answer.setQuestion(this);
+    }
 }
