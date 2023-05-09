9847631ed9958d6b3736a6bc83dc8caa840f7890
ddmkim94 <ddmkim94@gmail.com>
2023-05-09 14:19:46 +0900

    타임리프 캐시 비활성화

--- src/main/java/com/ll/sbb/question/QuestionController.java
+++ src/main/java/com/ll/sbb/question/QuestionController.java
@@ -2,17 +2,31 @@ package com.ll.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+
+        /**
+         * Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다.
+         * Model 객체에 값을 담아두면 템플릿에서 사용 가능
+          */
+
+        model.addAttribute("questionList", questionList);
+
         return "question_list";
     }
 }
