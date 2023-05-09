685a29662da05b56d425bf3bc41de8af80af1e3f
ddmkim94 <ddmkim94@gmail.com>
2023-05-09 15:10:42 +0900

    QuestionService 도입
    Controller - Service - Repository의 관계는 단방향 이여야 한다.
    양방향으로 설정되면 안됨.

--- src/main/java/com/ll/sbb/question/QuestionController.java
+++ src/main/java/com/ll/sbb/question/QuestionController.java
@@ -5,20 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
+// 컨트롤러는 Repository가 있는지 몰라야 한다.
+// 서비스는 웹브라우저라는것이 이 세상에 존재하는지 몰라야 한다.
+// 세션은 컨트롤러에서 처리해야 한다.
+// 리포지터리는 서비스를 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야 한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
 
         /**
          * Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다.
