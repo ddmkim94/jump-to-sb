c1c891283218604a46932595b88df78c51ccc423
ddmkim94 <ddmkim94@gmail.com>
2023-05-12 14:49:56 +0900

    게시글 정렬 조건 추가
    가장 최근 생성일 순으로 정렬
    생성일이 같은 경우에는 id를 기준으로 내림차순 정렬

--- src/main/java/com/ll/sbb/question/QuestionService.java
+++ src/main/java/com/ll/sbb/question/QuestionService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,8 +19,12 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
-        return questionRepository.findAllByOrderByIdDesc(pageable);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        sorts.add(Sort.Order.desc("id"));
+
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAll(pageable);
     }
 
     public List<Question> getList() {
