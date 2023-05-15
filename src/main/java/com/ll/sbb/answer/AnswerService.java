209166ee59b948967991542d0e602e93eb714be8
ddmkim94 <ddmkim94@gmail.com>
2023-05-15 20:19:11 +0900

    답변 수정 기능 구현

--- src/main/java/com/ll/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/sbb/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.ll.sbb.answer;
 
+import com.ll.sbb.exception.DataNotFoundException;
 import com.ll.sbb.question.Question;
 import com.ll.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,17 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setAuthor(author);
         question.addAnswer(answer);
+
+        answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Long id) {
+        return answerRepository.findById(id).orElseThrow(() -> new DataNotFoundException("답변을 찾을 수 없습니다."));
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
 
         answerRepository.save(answer);
     }
